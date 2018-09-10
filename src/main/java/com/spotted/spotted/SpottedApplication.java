4c57e2a592fea4ee15a81f93b75b646aae5c3d61
hemillainysantos <hemillainy.santos@ccc.ufcg.edu.br>
2018-09-10 18:52:31 -0300

    up projeto base

---
+++ src/main/java/com/spotted/spotted/SpottedApplication.java
@@ -0,0 +1,12 @@
+package com.spotted.spotted;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpottedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpottedApplication.class, args);
+	}
+}
