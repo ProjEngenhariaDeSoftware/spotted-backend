c9d3d1a0abf0e8ddbcb4746318263664bafe420c
geovanens <geovane.silva@ccc.ufcg.edu.br>
2018-10-17 14:03:22 -0300

    adicionar construtor, gets e sets de Post

--- src/main/java/com/spotted/models/Post.java
+++ src/main/java/com/spotted/models/Post.java
@@ -42,4 +42,63 @@ public class Post {
 	@NotNull(message = "Datetime can not be null")
 	@NotEmpty(message = "Datetime can not be empty")
 	private Date datetime;
+	
+	public Post() {}
+	
+	public Post(Long id, String username, PostTypes type, String text, Image image, Date datetime) {
+		this.id = id;
+		this.username = username;
+		this.type = type;
+		this.text = text;
+		this.image = image;
+		this.datetime = datetime;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public PostTypes getType() {
+		return type;
+	}
+
+	public void setType(PostTypes type) {
+		this.type = type;
+	}
+
+	public String getText() {
+		return text;
+	}
+
+	public void setText(String text) {
+		this.text = text;
+	}
+
+	public Image getImage() {
+		return image;
+	}
+
+	public void setImage(Image image) {
+		this.image = image;
+	}
+
+	public Date getDatetime() {
+		return datetime;
+	}
+
+	public void setDatetime(Date datetime) {
+		this.datetime = datetime;
+	}
 }
