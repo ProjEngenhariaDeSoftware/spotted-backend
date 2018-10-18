f3ac0b69a2f1836b76502a0eaa94c9f4f2d653b9
geovanens <geovane.silva@ccc.ufcg.edu.br>
2018-10-18 13:45:54 -0300

    adcionar base repositorio, controller e service de user

--- src/main/java/com/spotted/controllers/UserController.java
+++ src/main/java/com/spotted/controllers/UserController.java
@@ -1,5 +1,23 @@
 package com.spotted.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.*;
+
+import com.spotted.models.User;
+import com.spotted.repositories.UserRepository;
+
+@RestController
+@RequestMapping(value = "/api")
+@CrossOrigin(value = "*")
 public class UserController {
+	
+	@Autowired
+	UserRepository userRepository;
+	
+	@RequestMapping(value = "/user", method = RequestMethod.POST)
+	public User save(@RequestBody User user) {
+		return this.userRepository.save(user);
+	}
 
 }
--- src/main/java/com/spotted/services/UserService.java
+++ src/main/java/com/spotted/services/UserService.java
@@ -1,5 +1,19 @@
 package com.spotted.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.spotted.models.User;
+import com.spotted.repositories.UserRepository;
+
+@Service
 public class UserService {
+	
+	@Autowired
+    UserRepository userRepository;
+	
+	public User save(User user) {
+		return this.userRepository.save(user);
+	}
 
 }
