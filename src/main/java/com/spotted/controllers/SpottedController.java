0cb368c5552cf256a7cc8cdf6fc60ba0d02d776d
hemillainysantos <hemillainy.santos@ccc.ufcg.edu.br>
2018-10-18 14:21:43 -0300

    add construtor Spotted

--- src/main/java/com/spotted/controllers/SpottedController.java
+++ src/main/java/com/spotted/controllers/SpottedController.java
@@ -1,9 +1,15 @@
 package com.spotted.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
 import com.spotted.models.Spotted;
 import com.spotted.services.SpottedService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+
 
 @RestController
 @RequestMapping(value = "/api")
