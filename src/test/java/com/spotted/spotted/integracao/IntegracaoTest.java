1580b7545cbaef5c9cf2a6a39c398041cb361a94
cassioegc <cassio.cordeiro>
2018-10-18 23:28:12 -0300

    Teste de build

--- src/test/java/com/spotted/spotted/integracao/IntegracaoTest.java
+++ src/test/java/com/spotted/spotted/integracao/IntegracaoTest.java
@@ -17,11 +17,11 @@ public class IntegracaoTest {
 
     @Test
     public void test2() {
-        Assert.assertTrue(true);
+        Assert.assertTrue(false);
     }
 
     @Test
     public void test3() {
-        Assert.assertFalse(false);
+        Assert.assertFalse(true);
     }
 }
