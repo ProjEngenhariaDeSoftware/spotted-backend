17a3da4f5db4497d9ce997247ecfde614bcd48c3
cassioegc <cassio.cordeiro>
2018-10-18 22:09:01 -0300

    Ambiente de testes

---
+++ src/test/java/com/spotted/spotted/integracao/IntegracaoTest.java
@@ -0,0 +1,22 @@
+package com.spotted.spotted.integracao;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class IntegracaoTest {
+
+    @Test
+    public void test1() {
+        System.out.printf("Teste 1");
+    }
+
+    @Test
+    public void test2() {
+        Assert.assertTrue(true);
+    }
+}
