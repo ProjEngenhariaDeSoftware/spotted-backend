86e35f500791dbf43fe31e7bd2f2007fe5c78066
hemillainysantos <hemillainy.santos@ccc.ufcg.edu.br>
2018-10-25 15:20:04 -0300

    alterar tipo do commenter e tipo do auto incremento de Post

--- src/main/java/com/spotted/models/Post.java
+++ src/main/java/com/spotted/models/Post.java
@@ -25,7 +25,7 @@ public class Post {
 	
 	@Id
     @Column(name = "id")
-	@GeneratedValue(strategy = GenerationType.AUTO)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 	
 	@Column(name = "nickname")
