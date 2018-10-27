4bcf566cf36e1d3c59aae08a5ba5e92bb3dc4a30
cassioegc <cassio.cordeiro>
2018-10-27 00:13:47 -0300

    Testes

--- src/test/java/com/spotted/spotted/unidade/SpottedServiceTest.java
+++ src/test/java/com/spotted/spotted/unidade/SpottedServiceTest.java
@@ -10,24 +10,56 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.ArrayList;
 
+/**
+ * Testes de unidade para {@link SpottedService}
+ *
+ * @author cassioegc
+ */
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class SpottedServiceTest extends SpottedApplicationTests {
 
     @Autowired
-    SpottedService spottedService;
+    private SpottedService spottedService;
 
+    /**
+     * Testa se um spotted é salvo com sucesso.
+     */
     @Test
-    public void test() {
-        byte[] a = new byte[]{};
-        spottedService.save(new Spotted("Minha casa", "CC", "Gata", a , true));
-        System.out.println("!!!!!@@@@@@@@@@@@@@@((((((+++=======" + spottedService.getAll().toString());
+    public void testSave() {
+        Spotted spotted = new Spotted("Teste 1", "teste", "teste", new byte[]{} , true);
+        Spotted spottedSaved = spottedService.save(spotted);
+        Assert.assertTrue("O spotted deveria ter sido salvo.", spottedService.getAll().contains(spotted));
+        Assert.assertEquals("Os spotteds deveriam ser iguais", spottedSaved, spotted);
     }
 
+    /**
+     * Testa se um spotted com texto vazio não é salvo.
+     */
     @Test
-    public void test2() {
-        spottedService.save(new Spotted("Minha casa", "CC", "Gata", new byte[]{} , true));
-        Assert.assertTrue(true);
+    public void testSaveEmptyText() {
+        Spotted spotted = new Spotted("Teste 1", "teste", "", new byte[]{} , true);
+        try {
+            spottedService.save(spotted);
+            Assert.fail("Uma exceção deveria ter sido lançada.");
+        } catch (Exception e) {
+            Assert.assertFalse("O spotted não deveria ter sido salvo.", spottedService.getAll().contains(spotted));
+        }
+    }
+
+    /**
+     * Testa se um spotted com texto null não é salvo.
+     */
+    @Test
+    public void testSaveNullText() {
+        Spotted spotted = new Spotted("Teste 1", "teste", null, new byte[]{} , true);
+        try {
+            spottedService.save(spotted);
+            Assert.fail("Uma exceção deveria ter sido lançada.");
+        } catch (Exception e) {
+            Assert.assertFalse("O spotted não deveria ter sido salvo.", spottedService.getAll().contains(spotted));
+        }
     }
 }
