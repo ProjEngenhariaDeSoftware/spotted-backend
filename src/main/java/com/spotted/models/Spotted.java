93e182912752df7d027d74b8fab1894671e7c687
cassioegc <cassio.cordeiro>
2018-10-27 00:11:57 -0300

    Alterações necessárias aos testes

--- src/main/java/com/spotted/models/Spotted.java
+++ src/main/java/com/spotted/models/Spotted.java
@@ -2,10 +2,23 @@ package com.spotted.models;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
-import javax.persistence.*;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.Lob;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
 import javax.validation.constraints.NotEmpty;
 
 import javax.validation.constraints.NotNull;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Objects;
 import java.util.Set;
 
 @Entity
@@ -35,8 +48,8 @@ public class Spotted {
 	
 	@Column(name = "visible")
 	private boolean visible;
-	
-	@OneToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+
+	@OneToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
 	@JoinColumn(name = "spotted_id", referencedColumnName = "id")
 	private Set<Comment> comments;
 	
@@ -49,6 +62,7 @@ public class Spotted {
 		this.text = text;
 		this.image = image;
 		this.visible = visible;
+		this.comments = new HashSet<>();
 	}
 	
 	public Long getId() {
@@ -98,16 +112,37 @@ public class Spotted {
 	public void setVisible(boolean visible) {
 		this.visible = visible;
 	}
-	
+
 	public Set<Comment> getComments() {
 		return comments;
 	}
-	
+
 	public void setComments(Set<Comment> comments) {
 		this.comments = comments;
 	}
-	
+
 	public void addComment(Comment comment) {
 		this.comments.add(comment);
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (o == null || getClass() != o.getClass()) return false;
+		Spotted spotted = (Spotted) o;
+		return visible == spotted.visible &&
+				Objects.equals(id, spotted.id) &&
+				Objects.equals(location, spotted.location) &&
+				Objects.equals(course, spotted.course) &&
+				Objects.equals(text, spotted.text) &&
+				Arrays.equals(image, spotted.image) &&
+				Objects.equals(comments, spotted.comments);
+	}
+
+	@Override
+	public int hashCode() {
+		int result = Objects.hash(id, location, course, text, visible, comments);
+		result = 31 * result + Arrays.hashCode(image);
+		return result;
 	}
 }
