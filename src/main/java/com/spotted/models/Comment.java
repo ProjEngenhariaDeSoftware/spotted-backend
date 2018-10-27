2be9bc0b2659958a4b72e7003e9106b888c7fea9
cassioegc <cassio.cordeiro>
2018-10-27 13:31:42 -0300

    Ajustar imports

--- src/main/java/com/spotted/models/Comment.java
+++ src/main/java/com/spotted/models/Comment.java
@@ -1,6 +1,11 @@
 package com.spotted.models;
 
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import java.util.Objects;
