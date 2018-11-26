e8b2040d2eb97222952ab6f507775065ca764c81
cassioegc <cassio.cordeiro>
2018-11-26 14:19:01 -0300

    testes

--- src/test/java/com/spotted/spotted/unidade/NotificationTest.java
+++ src/test/java/com/spotted/spotted/unidade/NotificationTest.java
@@ -1,50 +1,51 @@
-//package com.spotted.spotted.unidade;
-//
-//import com.spotted.models.Notification;
-//import com.spotted.models.User;
-//import org.junit.Assert;
-//import org.junit.Before;
-//import org.junit.Test;
-//import org.junit.runner.RunWith;
-//import org.springframework.boot.test.context.SpringBootTest;
-//import org.springframework.test.context.junit4.SpringRunner;
-//
-///**
-// * Testes de unidade para {@link Notification}
-// */
-//@RunWith(SpringRunner.class)
-//@SpringBootTest
-//public class NotificationTest {
-//
-//    private Notification notification;
-//    private User user;
-//    @Before
-//    public void setUp() {
-//        this.user = new User("test@ccc", "NEWS", "");
-//        this.notification = new Notification("NEWS", 1L, user);
-//    }
-//
-//    /**
-//     * Testa se duas notificações são iguais.
-//     */
-//    @Test
-//    public void testEquals() {
-//        Notification notification = new Notification("NEWS", 1L, this.user);
-//        this.notification.setId(1L);
-//        notification.setId(1L);
-//        Assert.assertEquals("As notificações deveriam ser iguais.", this.notification, notification);
-//    }
-//
-//    /**
-//     * Testa se duas notificações com ids diferentes são diferentes.
-//     */
-//    @Test
-//    public void testEqualsDifferentesNotifications() {
-//        Notification notification = new Notification("NEWS", 1L, this.user);
-//        this.notification.setId(1L);
-//        notification.setId(2L);
-//        Assert.assertNotEquals("As notificações não deveriam ser iguais.", this.notification, notification);
-//    }
-//
-//
-//}
+package com.spotted.spotted.unidade;
+
+import com.spotted.models.Notification;
+import com.spotted.models.User;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+
+/**
+ * Testes de unidade para {@link Notification}
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class NotificationTest {
+
+    private Notification notification;
+    private User user;
+    @Before
+    public void setUp() {
+        this.user = new User("test@ccc", "NEWS", "");
+        this.notification = new Notification("NEWS", 1L, user, "");
+    }
+
+    /**
+     * Testa se duas notificações são iguais.
+     */
+    @Test
+    public void testEquals() {
+        Notification notification = new Notification("NEWS", 1L, this.user, "");
+        this.notification.setId(1L);
+        notification.setId(1L);
+        Assert.assertEquals("As notificações deveriam ser iguais.", this.notification, notification);
+    }
+
+    /**
+     * Testa se duas notificações com ids diferentes são diferentes.
+     */
+    @Test
+    public void testEqualsDifferentesNotifications() {
+        Notification notification = new Notification("NEWS", 1L, this.user, "");
+        this.notification.setId(1L);
+        notification.setId(2L);
+        Assert.assertNotEquals("As notificações não deveriam ser iguais.", this.notification, notification);
+    }
+
+
+}
--- src/test/java/com/spotted/spotted/unidade/SpottedTest.java
+++ src/test/java/com/spotted/spotted/unidade/SpottedTest.java
@@ -1,85 +1,85 @@
-//package com.spotted.spotted.unidade;
-//
-//import com.spotted.models.Comment;
-//import com.spotted.models.Spotted;
-//import com.spotted.models.User;
-//import org.junit.Assert;
-//import org.junit.Before;
-//import org.junit.Test;
-//import org.junit.runner.RunWith;
-//import org.springframework.boot.test.context.SpringBootTest;
-//import org.springframework.test.context.junit4.SpringRunner;
-//
-//import java.time.LocalDateTime;
-//import java.util.ArrayList;
-//
-///**
-// * Testes de unidade para {@link Spotted}
-// *
-// * @author cassioegc
-// */
-//@RunWith(SpringRunner.class)
-//@SpringBootTest
-//public class SpottedTest {
-//
-//    private Spotted spotted;
-//    private User user;
-//
-//    @Before
-//    public void setUp() {
-//        this.spotted = new Spotted("Test", "Curso Teste", "Testando", "", true);
-//        this.user = new User("teste@teste", "teste", "");
-//    }
-//
-//    /**
-//     * Testa a adição de um comentário a um spotted.
-//     */
-//    @Test
-//    public void testAddComment() {
-//        Comment comment = new Comment(new ArrayList<>(), "Comentário de teste", this.user, null);
-//        this.spotted.addComment(comment);
-//        Assert.assertTrue("O spotted deveria conter o comentário.",
-//                this.spotted.getComments().contains(comment));
-//    }
-//
-//    /**
-//     * Testa se dois spotteds são diferentes ao adicionar um comentário a um deles.
-//     */
-//    @Test
-//    public void testEqualsDifferentSpotteds() {
-//        Comment comment = new Comment(new ArrayList<>(), "Comentário de teste", this.user, null);
-//        Spotted spottedAux = this.spotted;
-//        this.spotted.addComment(comment);
-//        Assert.assertEquals("Os spotteds deveriam ser diferentes.",
-//                this.spotted, spottedAux);
-//
-//    }
-//
-//    /**
-//     * Testa se dois spotteds são iguais mesmo com visibilidades diferentes.
-//     */
-//    @Test
-//    public void testEquals() {
-//        Spotted spottedAux = new Spotted("Test", "Curso Teste", "Testando", "", false);
-//        Assert.assertEquals("Os spotteds deveriam ser iguais.",
-//                this.spotted, spottedAux);
-//    }
-//
-//    @Test
-//    public void testEqualsDifferentsDateTime() {
-//        Spotted spottedAux = new Spotted("Test", "Curso Teste", "Testando", "", true);
-//        spottedAux.setDatetime(LocalDateTime.of(2018, 11, 10, 11, 00));
-//        this.spotted.setDatetime(LocalDateTime.of(2019, 11, 10, 11, 00));
-//        Assert.assertNotEquals(this.spotted, spottedAux);
-//    }
-//
-//    /**
-//     * Testa a alteração de visibilidade do Spotted.
-//     */
-//    @Test
-//    public void testVisible() {
-//        this.spotted.setVisible(false);
-//        Assert.assertFalse("O spotted não deveria estar visível.", this.spotted.getVisible());
-//    }
-//
-//}
+package com.spotted.spotted.unidade;
+
+import com.spotted.models.Comment;
+import com.spotted.models.Spotted;
+import com.spotted.models.User;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+
+/**
+ * Testes de unidade para {@link Spotted}
+ *
+ * @author cassioegc
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpottedTest {
+
+    private Spotted spotted;
+    private User user;
+
+    @Before
+    public void setUp() {
+        this.spotted = new Spotted("Test", "Curso Teste", "Testando", "", true);
+        this.user = new User("teste@teste", "teste", "");
+    }
+
+    /**
+     * Testa a adição de um comentário a um spotted.
+     */
+    @Test
+    public void testAddComment() {
+        Comment comment = new Comment(new ArrayList<>(), "Comentário de teste", this.user, null);
+        this.spotted.addComment(comment);
+        Assert.assertTrue("O spotted deveria conter o comentário.",
+                this.spotted.getComments().contains(comment));
+    }
+
+    /**
+     * Testa se dois spotteds são diferentes ao adicionar um comentário a um deles.
+     */
+    @Test
+    public void testEqualsDifferentSpotteds() {
+        Comment comment = new Comment(new ArrayList<>(), "Comentário de teste", this.user, null);
+        Spotted spottedAux = this.spotted;
+        this.spotted.addComment(comment);
+        Assert.assertEquals("Os spotteds deveriam ser diferentes.",
+                this.spotted, spottedAux);
+
+    }
+
+    /**
+     * Testa se dois spotteds são iguais mesmo com visibilidades diferentes.
+     */
+    @Test
+    public void testEquals() {
+        Spotted spottedAux = new Spotted("Test", "Curso Teste", "Testando", "", false);
+        Assert.assertEquals("Os spotteds deveriam ser iguais.",
+                this.spotted, spottedAux);
+    }
+
+    @Test
+    public void testEqualsDifferentsDateTime() {
+        Spotted spottedAux = new Spotted("Test", "Curso Teste", "Testando", "", true);
+        spottedAux.setDatetime(LocalDateTime.of(2018, 11, 10, 11, 00));
+        this.spotted.setDatetime(LocalDateTime.of(2019, 11, 10, 11, 00));
+        Assert.assertNotEquals(this.spotted, spottedAux);
+    }
+
+    /**
+     * Testa a alteração de visibilidade do Spotted.
+     */
+    @Test
+    public void testVisible() {
+        this.spotted.setVisible(false);
+        Assert.assertFalse("O spotted não deveria estar visível.", this.spotted.getVisible());
+    }
+
+}
